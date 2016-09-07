278993f673b3b115629aaf5ed2460a1d1885db4e
jerry-0824 <63935127@qq.com>
2016-09-07 15:29:20 +0800

    Add DeleteGroup.java.

---
+++ chapter_14_ZooKeeper/src/DeleteGroup.java
@@ -0,0 +1,35 @@
+/**
+ * Filename: DeleteGroup.jsp
+ * Author:   jerry_0824
+ * Email:    63935127#qq.com
+ * Date:     2016-09-07
+ * Time:     8:06
+ * Version:  v1.0.0
+ */
+public class DeleteGroup extends ConnectionWatcher {
+    public void delete(String groupName) throws keeperException, InterruptedException {
+        String path = "/" + groupName;
+
+        try {
+            List<String> children = zk.getChildren(path, false);
+
+            for (String child : children) {
+                zk.delete(path + "/" + child, -1);
+            }
+
+            zk.delete(path, -1);
+        } catch (KeeperException.NoNodeException e) {
+            System.out.printf("Group %s does not exist\n", groupName);
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        DeleteGroup deleteGroup = new DeleteGroup();
+
+        deleteGroup.connect(args[0]);
+        deleteGroup.delete(args[1]);
+
+        deleteGroup.close();
+    }
+}
