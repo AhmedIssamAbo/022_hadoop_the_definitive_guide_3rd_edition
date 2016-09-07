5a2f9b77d3664dbdfd5e17ef449ff24a806e2d7f
jerry-0824 <63935127@qq.com>
2016-09-07 15:28:54 +0800

    Add ConnectionWatcher.java.

---
+++ chapter_14_ZooKeeper/src/ConnectionWatcher.java
@@ -0,0 +1,34 @@
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Filename: ConnectionWatcher.jsp
+ * Author:   jerry_0824
+ * Email:    63935127#qq.com
+ * Date:     2016-09-07
+ * Time:     7:49
+ * Version:  v1.0.0
+ */
+public class ConnectionWatcher implements Watcher {
+    private static final int SESSION_TIMEOUT = 5000;
+
+    protected ZooKeeper zk;
+    private CountDownLatch connectedSignal = new CountDownLatch(1);
+
+    public void connect(String hosts) throws IOException, InterruptedException {
+        zk = new ZooKeeper(hosts, SESSION_TIMEOUT, this);
+        connectedSignal.await();
+    }
+
+    @Override
+    public void process(WatchedEvent event)
+    {
+        if (event.getState() == KeeperState.SyncConnected) {
+            connectedSignal.countDown();
+        }
+    }
+
+    public void close() throws InterruptedException {
+        zk.close();
+    }
+}
