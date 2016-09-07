c883a92eeef7e72e8ba9c60f4ff6e7b8b2430f4b
jerry-0824 <63935127@qq.com>
2016-09-07 14:02:30 +0800

    Add ConfigUpdater.java.

---
+++ chapter_14_ZooKeeper/src/ConfigUpdater.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Filename: ConfigUpdater.jsp
+ * Author:   jerry_0824
+ * Email:    63935127#qq.com
+ * Date:     2016-09-07
+ * Time:     8:11
+ * Version:  v1.0.0
+ */
+public class ConfigUpdater {
+    public static final String PATH = "/config";
+
+    private ActiveKeyValueStore store;
+    private Random random = new Random();
+
+    public ConfigUpdater(String hosts) throws IOException, InterruptedException {
+        store = new ActiveKeyValueStore();
+        store.connect(hosts);
+    }
+
+    public void run() throws InterruptedException, KeeperException {
+        while(true) {
+            String value = random.nextInt(100) + '';
+            store.write(PATH, value);
+            System.out.printf("Set %s to %s\n", PATH, value);
+
+            TimeUnit.SECONDS.sleep(random.nextInt(10));
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        ConfigUpdater configUpdater = new ConfigUpdater(args[0]);
+
+        configUpdater.run();
+    }
+}
