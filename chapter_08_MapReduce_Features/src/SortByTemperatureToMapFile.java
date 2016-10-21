b635fbce527f4bef69e6a8257644be38b6ca89a4
jerry-0824 <63935127@qq.com>
2016-10-21 16:53:50 +0800

    Add SortByTemperatureToMapFile.java.

---
+++ chapter_08_MapReduce_Features/src/SortByTemperatureToMapFile.java
@@ -0,0 +1,45 @@
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.compress.GzipCodec;
+import org.apache.hadoop.mapred.MapFileOutputFormat;
+import org.apache.hadoop.mapred.SequenceFileInputFormat;
+import org.apache.hadoop.mapred.SequenceFileOutputFormat;
+import org.apache.hadoop.mapred.jobcontrol.Job;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.quartz.JobBuilder;
+
+/**
+ * Filename: SortByTemperatureToMapFile.java
+ * Author:   jerry_0824
+ * Email:    63935127#qq.com
+ * Date:     2016-09-08
+ * Time:     20:46
+ * Version:  v1.0.0
+ */
+public class SortByTemperatureToMapFile extends Configured implements Tool {
+    @Override
+    public int run(String[] args) throws Exception {
+        Job job = JobBuilder.parseInputAndOutput(this, getConf(), args);
+        if (null == job) {
+            return -1;
+        }
+
+        job.setInputFormatClass(SequenceFileInputFormat.class);
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputFormatClass(MapFileOutputFormat.class);
+
+        SequenceFileOutputFormat.setCompressOutput(job, true);
+        SequenceFileOutputFormat.setOutputCompressorClass(job, GzipCodec.class);
+        SequenceFileOutputFormat.setOutputCompressionType(job, SequenceFile.CompressionType.BLOCK);
+
+        return job.waitForCompletion(true) ? 0 : 1;
+    }
+
+    public static void main(String[] args) throws Exception {
+        int exitCode = ToolRunner.run(new SortDataPreprocessor(), args);
+        System.exit(exitCode);
+    }
+}
+
